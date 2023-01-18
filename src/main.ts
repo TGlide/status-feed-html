995daffdca237e2b0863f70c2e89bcb6a0b5fbde
tglide <26071571+TGlide@users.noreply.github.com>
2023-01-18 20:45:04 +0000

    :tada: initial commit

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import "@fontsource/inter/latin-400.css";
+import "@fontsource/inter/latin-600.css";
+
+import "@fontsource/poppins/latin-500.css";
+import "@fontsource/poppins/latin-600.css";
+import "@fontsource/poppins/latin-700.css";
+
+import "@fontsource/source-code-pro/latin-400.css";
